4b047b9035d3e6741c01cd4247427662b217068a
jjokela <jarmo.jokela@gmail.com>
2016-06-05 17:16:54 +0200

    Added support for VSCode, added publish location

--- ng2old/systemjs.config.js
+++ ng2old/systemjs.config.js
@@ -9,10 +9,9 @@
     var map = {
         'app': 'app', // 'dist',
         'api': 'api',
-        'rxjs': 'node_modules/rxjs',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        '@angular': 'node_modules/@angular',
-        '@angular2-material': 'node_modules/@angular2-material'
+        'rxjs': 'lib/rxjs',
+        'angular2-in-memory-web-api': 'lib/angular2-in-memory-web-api',
+        '@angular': 'lib/@angular'
     };
 
     // packages tells the System loader how to load when no filename and/or no extension
